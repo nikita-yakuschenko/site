1d1d0678a9f031286a58541d25a155fc2ee98be8
indy_x <nikita.yakuschenko@gmail.com>
2026-09-24 18:12:11 +0300

    Update preparation card illustrations and caption

--- src/app/manufacture/page.tsx
+++ src/app/manufacture/page.tsx
@@ -90,10 +90,10 @@ export default function ManufacturePage() {
             <ol className="mortgage-steps manufacture-prep__cards">
               {["Несущий каркас", "Плитные материалы", "Покраска"].map((title, index) => (
                 <li key={title}>
-                  <Image className="manufacture-prep__card-image" src={index === 0 ? "/img/cards/wbz.png" : index === 1 ? "/img/cards/format.png" : "/img/cards/imitation2.png"} alt="" width={320} height={180} sizes="(max-width: 719px) 60vw, 320px" unoptimized />
+                  <Image className="manufacture-prep__card-image" src={index === 0 ? "/img/cards/logs.png" : index === 1 ? "/img/cards/plate.png" : "/img/cards/imitation2.png"} alt="" width={320} height={180} sizes="(max-width: 719px) 60vw, 320px" unoptimized />
                   <strong>{index === 0 ? <>Несущий<br />каркас</> : index === 1 ? <>Плитные<br />материалы</> : title}</strong>
                   <p className="manufacture-prep__caption">
-                    {index === 0 ? <><span>Детали каркаса</span><span>вырезаются на станке с ЧПУ</span></> : index === 1 ? <><span>Листы OSB и GTS разрезаются</span><span>на станке точно в размер</span></> : <><span>Отделочные материалы</span><span>окрашиваются</span><span>на специальной линии</span></>}
+                    {index === 0 ? <><span>Детали каркаса вырезаются</span><span>на станке с ЧПУ</span></> : index === 1 ? <><span>Листы OSB и GTS разрезаются</span><span>на станке точно в размер</span></> : <><span>Отделочные материалы</span><span>окрашиваются</span><span>на специальной линии</span></>}
                   </p>
                 </li>
               ))}
